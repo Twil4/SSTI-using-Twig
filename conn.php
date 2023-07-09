6c06cacea89cbf905e58d02b73ff927e0fd3f05d
audwc28 <auducis28@gmail.com>
2023-07-09 18:22:59 +0700

    first comment

---
+++ conn.php
@@ -0,0 +1,4 @@
+<?php
+   $connect = mysqli_connect('localhost', 'root', '', 'twig');
+   mysqli_set_charset($connect, 'utf8');
+?>
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php 
+
+require 'vendor/autoload.php';
+require 'conn.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('views');
+$twig = new \Twig\Environment($loader);
+
+/*$md5Filter = new \Twig\TwigFilter('md5', function($string){
+	return md5($string);
+});*/
+
+/*$twig->addFilter($md5Filter);*/
+
+$lexer = new \Twig\Lexer($twig, array(
+	'tag_block'    => array('{', '}'),
+	'tag_variable' => array('{{', '}}')
+));
+
+$twig->setLexer($lexer);
+
+$sql = "SELECT * FROM meo";
+$result = mysqli_query($connect, $sql);
+$meoArray = array();
+if(mysqli_num_rows($result) > 0) {
+	while($row = mysqli_fetch_assoc($result)){
+		$meo = array(
+			'ID' => $row['ID'],
+			'name' => $row['name'],
+			'content' => $row['content'],
+			'link' => $row['link']
+		);
+		$meoArray[] = $meo;
+	}
+}
+
+if(empty($_GET)){
+	echo $twig->render('meo.html',[ 
+		'meoArray' => $meoArray
+	]);
+}
